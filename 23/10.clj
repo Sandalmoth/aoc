1105965395ad210747a3b2ef98f01694c0ebb3c6
Jonathan <sandalmoth@pm.me>
2023-12-10 19:28:53 +0100

    unsuccessful attempt at part 2

--- 23/10.clj
+++ 23/10.clj
@@ -61,3 +61,39 @@
 (def pipe-loop (conj (follow-path [101 52] :w []) start-pipe-xy))
 (println pipe-loop)
 (println (count pipe-loop)) ; div by 2 to get answer (6823)
+
+
+; one way of checking for being inside a shape is 
+; to see how many crossings there are on the way out
+; and if it's odd we're inside
+; the alignment of gaps and pipes makes ita awkward
+; but maybe we can just move horizontally and ignore - on the path
+
+(defn not-h? [x] (not (or (= x \-) (= x \S))))
+
+(def pipe-loop-set (set (filter #(not-h? (pipe-at %)) pipe-loop)))
+
+(println (contains? pipe-loop-set [100 52]))
+
+
+(defn count-left [xy]
+  (let [[x y] xy]
+    (reduce + (map #(if % 1 0) 
+      (map #(contains? pipe-loop-set [% y]) (range x))
+      ))))
+
+(println (count-left start-pipe-xy))
+
+; (println (mapv #(vec [%1 %2]) (range 3) (range 3)))
+(println (for [a (range 3) b (range 3)] [a b]))
+
+(def candidates (filter #(not (contains? pipe-loop-set %)) (for [a (range 140) b (range 140)] [a b])))
+; (println candidates)
+
+(def inside (filter #(odd? (count-left %)) candidates))
+(def outside (filter #(even? (count-left %)) candidates))
+(println (count inside))
+
+
+
+
